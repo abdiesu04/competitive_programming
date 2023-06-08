6b806728a5aff2103c3ff75e0fd74f9cf1c720fa
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-06-08 09:51:06 +0300

    Create NestedList.py

---
+++ NestedList.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    students = []
+    
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name,score])
+  
+    scores = list(set([x[1] for x in students]))
+    scores.sort()
+    second_min = scores[1]
+    min_name = [x[0] for x in students if x[1] == second_min]
+    min_name.sort()
+    for i in min_name:
+        print(i)
+    

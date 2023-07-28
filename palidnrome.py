8210653b471431bc5acae83c106c7264de00d6a0
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-07-28 19:48:36 +0300

    Update and rename palidnrome.py to Valid_palidnrome.py

--- palidnrome.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        s = ''.join(c for c in s if c.isalnum()).lower()
-        return s == s[::-1]
-        """
-        :type s: str
-        :rtype: bool
-        """

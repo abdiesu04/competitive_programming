78942e7fb701d82c0c38cc3002e21a8c6eade6b1
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-10-22 15:20:25 +0300

    Time: 138 ms (86.28%), Space: 17.9 MB (63.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,7 +1,14 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        nums.sort(key=lambda x: x == 0)
-        return nums
+        index = 0
+
+        # Place non-zero elements at the start of the list
+        for num in nums:
+            if num != 0:
+                nums[index] = num
+                index += 1
+
+        # Fill the remaining positions with zeroes
+        while index < len(nums):
+            nums[index] = 0
+            index += 1

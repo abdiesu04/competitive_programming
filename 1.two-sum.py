4ef7db946b088bd29bda44b27b8d6a09e157ad3d
Abdi Esayas <abdiesayasbayisa@gmail.com>
2023-12-04 09:40:39 +0300

    two sum

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,6 +7,30 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+            
+            # Brute Force
+            # for i in range(len(nums)):
+            #     for j in range(i+1, len(nums)):
+            #         if nums[i] + nums[j] == target:
+            #             return [i, j]
+    
+            # Hash Table
+            # hash_table = {}
+            # for i in range(len(nums)):
+            #     if nums[i] in hash_table:
+            #         return [hash_table[nums[i]], i]
+            #     else:
+            #         hash_table[target - nums[i]] = i
+    
+            # Hash Table (One-pass)
+            hash_table = {}
+            for i in range(len(nums)):
+                if nums[i] in hash_table:
+                    return [hash_table[nums[i]], i]
+                else:
+                    hash_table[target - nums[i]] = i
+    
+            return [-1, -1]
         
 # @lc code=end
 

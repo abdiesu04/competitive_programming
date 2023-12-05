81ed41396ce7cb02ea64ff8157817199ce4ecd5c
Abdi Esayas <abdiesayasbayisa@gmail.com>
2023-12-05 12:33:18 +0300

    3 sum optimized

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -5,8 +5,8 @@
 #
 
 # @lc code=start
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
             
             # Brute Force
             # nums.sort()
@@ -20,26 +20,26 @@ class Solution:
             # return res
             
             # Two Pointers
-            nums.sort()
-            res = []
-            for i in range(len(nums)):
-                if i == 0 or nums[i] != nums[i-1]:
-                    l, r = i+1, len(nums)-1
-                    while l < r:
-                        s = nums[i] + nums[l] + nums[r]
-                        if s < 0:
-                            l += 1
-                        elif s > 0:
-                            r -= 1
-                        else:
-                            res.append([nums[i], nums[l], nums[r]])
-                            while l < r and nums[l] == nums[l+1]: 
-                                l += 1
-                            while l < r and nums[r] == nums[r-1]: 
-                                r -= 1
-                            l += 1
-                            r -= 1
-            return res
+            # nums.sort()
+            # res = []
+            # for i in range(len(nums)):
+            #     if i == 0 or nums[i] != nums[i-1]:
+            #         l, r = i+1, len(nums)-1
+            #         while l < r:
+            #             s = nums[i] + nums[l] + nums[r]
+            #             if s < 0:
+            #                 l += 1
+            #             elif s > 0:
+            #                 r -= 1
+            #             else:
+            #                 res.append([nums[i], nums[l], nums[r]])
+            #                 while l < r and nums[l] == nums[l+1]: 
+            #                     l += 1
+            #                 while l < r and nums[r] == nums[r-1]: 
+            #                     r -= 1
+            #                 l += 1
+            #                 r -= 1
+            # return res
             
             # Two Pointers (Optimized)
             # nums.sort()
@@ -64,6 +64,32 @@ class Solution:
             # return res
             
             #
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums)-2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l = i + 1
+            r = len(nums) -1
+            while l < r:
+                threeSum  = nums[i] + nums[r] + nums[l]
+                if threeSum < 0:
+                    l += 1
+                elif threeSum > 0:
+                    r -=   1
+                else:
+                    triplet = [nums[i], nums[l], nums[r]]
+                    res.append(triplet)
+                    while l < r and nums[l] == triplet[1]:
+                        l += 1
+                    while l < r and nums[r] == triplet[2]:
+                        r -= 1
+        return res
+                    
         
 # @lc code=end
 

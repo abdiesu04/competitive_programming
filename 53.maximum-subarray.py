ea4ed8e7bcc799a006d9d5ea156553723e7f9a1d
Abdi Esayas <abdiesayasbayisa@gmail.com>
2023-12-05 12:40:52 +0300

    maximum sum Brute Force

---
+++ 53.maximum-subarray.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=53 lang=python3
+#
+# [53] Maximum Subarray
+#
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # res = []
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)+1):
+        #         res.append(sum(nums[i:j]))
+        # return max(res)
+        #optimize
+        # res = []
+        
+        
+# @lc code=end
+

753c47519adf26827237a30b8c0fba06e0e12401
Abdi Esayas <abdiesayasbayisa@gmail.com>
2023-12-09 13:28:17 +0300

    largestOddNumber

--- 238.product-of-array-except-self.py
+++ 238.product-of-array-except-self.py
@@ -5,17 +5,23 @@
 #
 
 # @lc code=start
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1]*(len(nums))
-        prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums)-1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         res = [1]*(len(nums))
+#         prefix = 1
+#         for i in range(len(nums)):
+#             res[i] = prefix
+#             prefix *= nums[i]
+#         postfix = 1
+#         for i in range(len(nums)-1, -1, -1):
+#             res[i] *= postfix
+#             postfix *= nums[i]
+#         return res
+
+nums = [1,2,3,4]
+for i in range(len(nums)+1):
+    for j in range(i+1, len(nums)+1):
+        print(nums[i:j])
+
 # @lc code=end
 

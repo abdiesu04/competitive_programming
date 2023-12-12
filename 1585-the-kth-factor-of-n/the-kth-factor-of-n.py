3bb597eb87a68ca604d80f4ea3598d4dd8010912
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-12 11:04:09 +0300

    Time: 40 ms (53.79%) | Memory: 16.1 MB (81.12%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -1,10 +1,16 @@
-
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        fact_count = 0
-        for i in range(1,n+1):
-            if (n%i == 0):
-                fact_count += 1
-                if fact_count == k:
-                    return i
-        return -1
+        factor = 1
+
+        for i in range(1, n + 1):
+            if n % i == 0:
+                factor = i
+                k -= 1
+            if factor == n and k > 0:
+                return -1
+            if k == 0:
+                return factor
+
+        return factor
+
+        

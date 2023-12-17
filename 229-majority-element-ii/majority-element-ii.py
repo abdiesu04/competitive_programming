08071db48043b9ed70c961f6279b1e58b503c4f9
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-18 00:47:59 +0300

    Time: 115 ms (33.46%) | Memory: 17.8 MB (49.16%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        freq={}
-        answer=[]
-        for num in nums:
-            freq[num]=freq.get(num,0)+1
-            if freq[num] == len(nums)//3+1:
-                answer.append(num)
-        return answer
+        c = Counter(nums)
+        res = []
+        n = len(nums)
+        for k,v in c.items():
+            if v > n//3:
+                res.append(k)
+        return res

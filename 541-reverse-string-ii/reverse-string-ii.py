4941f36635417937e5278922ad7f93a03693808b
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-22 17:11:18 +0300

    Time: 43 ms (42.19%) | Memory: 17.6 MB (5.97%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -1,18 +1,14 @@
 # class Solution:
 #     def reverseStr(self, s: str, k: int) -> str:
-#         if k > len(s):
-#             return s[::-1]
-#         s = list(s)
 #         res = []
 #         for i in range(0, len(s), 2 * k):
-#             res.append(s[i:i+k][::-1])
-#             res.append(s[i+k:i+2*k])
+#             res.append(''.join(s[i:i+k][::-1]))
+#             res.append(''.join(s[i+k:i+2*k]))
 #         return ''.join(res)
 
 class Solution:
-    def reverseStr(self, s: str, k: int) -> str:
-        res = []
-        for i in range(0, len(s), 2 * k):
-            res.append(''.join(s[i:i+k][::-1]))
-            res.append(''.join(s[i+k:i+2*k]))
-        return ''.join(res)
+    def reverseStr(self, s:str, k : int) -> str:
+        s = list(s)
+        for i in range (0, len(s)-1 , 2 * k):
+            s[i:k+i] = s[i:k+i][::-1]
+        return ''.join(s)

760da88382e66d00ee59d0c6daecdc8115574410
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-22 17:27:22 +0300

    Time: 45 ms (16.43%) | Memory: 17.2 MB (20.06%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        l , r  = 0 , len(s)-1
+        while l < r:
+            if not s[l].isalpha():
+                l += 1
+            elif not s[r].isalpha():
+                r -= 1
+            else:
+                s[l] , s[r] = s[r] , s[l]
+                l += 1
+                r -= 1
+        return ''.join(s)
+        

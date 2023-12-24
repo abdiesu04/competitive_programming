04c19d19e9cf06d91de85be0a55fad8a89d50579
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-24 16:57:49 +0300

    Time: 46 ms (82.89%) | Memory: 17.4 MB (10.76%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,21 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        m = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+        # start with ans = 0
+        #iterate thourgh the string
+        #if the next elmnt is greater subtract from and else add to ans
+        # return ans
+        map = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
-        
         ans = 0
-        
-        for i in range(len(s)):
-            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
-                ans -= m[s[i]]
+
+        for i in range(len(s)-1):
+            if map[s[i+1]] > map[s[i]]:
+                ans -= map[s[i]]
             else:
-                ans += m[s[i]]
-        
-        return ans
+                ans += map[s[i]]
+        return ans + map[s[-1]]
+

69c49371643967cafa1ce17eb0e4ef53461292ff
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-24 17:37:06 +0300

    Time: 47 ms (40.10%) | Memory: 17.3 MB (17.75%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,10 +1,31 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        h = {ch: i for i, ch in enumerate(order)}
-        prev = list(h[ch] for ch in words[0])
-        for i in range(1, len(words)):
-            cur = list(h[ch] for ch in words[i])
-            if cur < prev:
-                return False
-            prev = cur
+        # Time: O(len(words) * avg word length)
+        # Space: O(len(words))
+        
+        weights = defaultdict(int)
+        num = 0
+        for i in order:
+            weights[i] = num
+            num += 1
+
+        for i in range(len(words) - 1):
+            # compare adjacent words, words[i] and words[i + 1]
+            for j in range(len(words[i])):
+                if j + 1 > len(words[i + 1]):
+                    # all letters same so far but second word is shorter
+                    return False
+                elif weights[words[i][j]] < weights[words[i + 1][j]]:
+                    # lexicographically sorted, break out of cur loop early
+                    break
+                elif weights[words[i][j]] == weights[words[i + 1][j]]:
+                    # same letters so far, keep comparing
+                    continue
+                else:
+                    # found 1 case where two words are not sorted
+                    return False
+        
+        # all adjacent words have been compared & nothing returned false
         return True
+            
+        

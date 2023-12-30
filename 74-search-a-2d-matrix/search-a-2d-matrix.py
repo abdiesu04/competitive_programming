b25db1ff7078bc655b125c167306ed67d40b21f8
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-30 03:39:32 +0300

    Time: 55 ms (21.30%) | Memory: 18 MB (7.28%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,8 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == target:
-                    return True
+        r = len(matrix)
+        c = len(matrix[0])
+        low = 0
+        high = (r * c) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if matrix[mid // c][mid % c] < target:
+                low = mid + 1
+            elif matrix[mid // c][mid % c] > target:
+                high = mid - 1
+            else:
+                return True
+
         return False
-        

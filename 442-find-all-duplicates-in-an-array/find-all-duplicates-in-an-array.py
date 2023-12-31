fa8aeb21b35697a2ee1dc4083b72d3b6bb552435
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-31 11:23:15 +0300

    Time: 439 ms (5.52%) | Memory: 27.5 MB (5.72%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,11 +1,24 @@
+# solved using constant extra space
+
+# class Solution:
+#     def findDuplicates(self, nums: List[int]) -> List[int]:
+#         res = []
+#         for n in nums:
+#             i = abs(n) -  1
+#             if nums[i] > 0:
+#                 nums[i] = -1 * nums[i]
+#             else:
+#                 res.append(i + 1)
+#         return res
+        
+
+#using set
+
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        res = []
-        for n in nums:
-            i = abs(n) -  1
-            if nums[i] > 0:
-                nums[i] = -1 * nums[i]
-            else:
-                res.append(i + 1)
-        return res
-        
+        res = set([])
+        cur = set()
+        for num in nums:
+            if num in cur: res.add(num)
+            else: cur.add(num)
+        return res    

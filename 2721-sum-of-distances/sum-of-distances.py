9e10f80d3586e87749b0df26fd44afa7f8aa40f0
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-31 13:37:01 +0300

    Time: 990 ms (5.03%) | Memory: 50.9 MB (33.67%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.py
+++ 2721-sum-of-distances/sum-of-distances.py
@@ -1,20 +1,19 @@
 class Solution:
     def distance(self, nums: List[int]) -> List[int]:
-        mp=defaultdict(list)
-        for i,j in enumerate(nums):
-            mp[j].append(i)
-        
-        ans=[0]*len(nums)
-        for key,val in mp.items():
-            # for key,val in d1.items():
-            suffixSum=sum(val)#suffix sum
-            prefixSum=0#prefix sum
-            suffixLength=len(val)#len(suffix)
-            prefixLength=0#len(pre)
-            for i in val:#iterate in the indexes value like [0,2,3]:
-                prefixSum+=i 
-                prefixLength+=1
-                suffixSum-=i
-                suffixLength-=1
-                ans[i]=(-prefixSum+prefixLength*i-suffixLength*i+suffixSum) #ans[0]=(-0+1*0-2*0+5)=5
-        return ans
+        distances = defaultdict(list)
+        for index, num in enumerate(nums):
+            distances[num].append(index) 
+        output = [0] * len(nums)
+        for values in distances.values():
+            n = len(values)
+            if n > 1:
+                rSum = sum(values)
+                lSum = 0
+                prevVal = 0
+                for i, val in enumerate(values):
+                    dist = val - prevVal
+                    rSum -= (n-i) * dist
+                    lSum += dist * i
+                    prevVal = val
+                    output[val] = rSum + lSum
+        return output

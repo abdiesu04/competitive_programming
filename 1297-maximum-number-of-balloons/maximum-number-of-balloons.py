1c2d8fc677cc80a9a119497d2d0ce55c10637239
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2023-12-31 13:54:32 +0300

    Time: 73 ms (5.56%) | Memory: 17.3 MB (9.71%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,25 +1,11 @@
-# class Solution:
-#     def maxNumberOfBalloons(self, text: str) -> int:
-#         mapp = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
-#         for char in text:
-#             if char in mapp:
-#                 mapp[char] += 1
-
-#         mapp['l'] //= 2
-#         mapp['o'] //= 2
-
-#         return min(mapp.values())
-
-from collections import Counter
-
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        text_count = Counter(text)
-        balloon_count = Counter("balloon")
+        mapp = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+        for char in text:
+            if char in mapp:
+                mapp[char] += 1
 
-        res = float('inf')
-        s = "balloon"
-        for char in s:
-            res = min(res, text_count[char] // balloon_count[char])
+        mapp['l'] //= 2
+        mapp['o'] //= 2
 
-        return res
+        return min(mapp.values())

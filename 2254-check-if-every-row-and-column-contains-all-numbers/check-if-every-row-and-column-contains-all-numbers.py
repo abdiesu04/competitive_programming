19b927e168f84a861c78dd39729028fd48c710b6
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-01-02 00:58:56 +0300

    Time: 1166 ms (5.06%) | Memory: 17.8 MB (29.29%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
@@ -1,22 +1,32 @@
-class Solution:
-    def checkValid(self, matrix: List[List[int]]) -> bool:
-        col = defaultdict(set)
-        row = defaultdict(set)
-        n = len(matrix)
+# class Solution:
+#     def checkValid(self, matrix: List[List[int]]) -> bool:
+#         col = defaultdict(set)
+#         row = defaultdict(set)
+#         n = len(matrix)
 
-        for r in range(n):
-            for c in range(n):
-                m = matrix[r][c]
-                col[c].add(m)
-                row[r].add(m)
+#         for r in range(n):
+#             for c in range(n):
+#                 m = matrix[r][c]
+#                 col[c].add(m)
+#                 row[r].add(m)
 
-        for i in col.values():
-            if len(i) < n:
-                return False
+#         for i in col.values():
+#             if len(i) < n:
+#                 return False
 
-        for i in row.values():
-            if len(i) < n:
-                return False
-        return True
+#         for i in row.values():
+#             if len(i) < n:
+#                 return False
+#         return True
 
         
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        check = {i for i in range(1, len(matrix) + 1)}
+        for row in matrix:
+            if set(row) != check:
+                return False
+        for column in zip(*matrix):
+            if set(column) != check:
+                return False
+        return True

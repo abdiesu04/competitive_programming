8bd9361cc519ab22e0852bc20457799681773b9d
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-01-05 20:29:36 +0300

    Time: 825 ms (94.82%) | Memory: 33.2 MB (30.59%) - LeetSync

--- 2145-grid-game/grid-game.py
+++ 2145-grid-game/grid-game.py
@@ -1,7 +1,8 @@
+
 # class Solution:
 #     def gridGame(self, grid: List[List[int]]) -> int:
 #         n = len(grid[0])
-#         preRow1 , preRow2 = grid[0].copy(), grid[1].copy()
+#         preRow1, preRow2 = grid[0].copy(), grid[1].copy()
 
 #         for i in range(1, n):
 #             preRow1[i] += preRow1[i-1]
@@ -11,25 +12,20 @@
 
 #         for i in range(n):
 #             top = preRow1[-1] - preRow1[i]
-#             bottom = preRow2[:i]
+#             bottom = preRow2[i -1] if i > 0 else 0
 #             secondRobot = max(top, bottom)
-#             res = min(res , secondRobot)
+#             res = min(res, secondRobot)
 #         return res
 
-class Solution:
-    def gridGame(self, grid: List[List[int]]) -> int:
-        n = len(grid[0])
-        preRow1, preRow2 = grid[0].copy(), grid[1].copy()
 
-        for i in range(1, n):
-            preRow1[i] += preRow1[i-1]
-            preRow2[i] += preRow2[i-1]
 
-        res = float('inf')
+class Solution(object):
+    def gridGame(self, grid):
+        result = float("inf")
+        left, right = 0, sum(grid[0])
 
-        for i in range(n):
-            top = preRow1[-1] - preRow1[i]
-            bottom = preRow2[i -1] if i > 0 else 0
-            secondRobot = max(top, bottom)
-            res = min(res, secondRobot)
-        return res
+        for a, b in zip(grid[0], grid[1]):
+            right -= a
+            result = min(result, max(left, right))
+            left += b
+        return result

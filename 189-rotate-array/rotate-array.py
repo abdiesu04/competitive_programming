1498414425556988f2e1913cc2173de4e2ca2746
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-01-11 00:39:03 +0300

    Time: 163 ms (85.57%) | Memory: 28.8 MB (25.38%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,8 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        res = [0] *len(nums)
-        for i in range(len(nums)):
-            res[(i+k)%len(nums)] = nums[i]
-        nums[:] = res
-        
+        # res = [0] *len(nums)
+        # for i in range(len(nums)):
+        #     res[(i+k)%len(nums)] = nums[i]
+        # nums[:] = res
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        n = len(nums)
+        k %= n  # Ensure k is within the range of list length
+
+        reverse(0, n - 1)  # Reverse the entire list
+        reverse(0, k - 1)  # Reverse the first k elements
+        reverse(k, n - 1)  # Reverse the remaining elements

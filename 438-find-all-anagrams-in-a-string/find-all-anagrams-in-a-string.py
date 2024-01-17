0e12d69bcd173219835511265bbaa2494efec8d6
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-01-17 20:58:58 +0300

    Time: 294 ms (28.27%) | Memory: 18.5 MB (37.12%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,35 +1,21 @@
-from collections import defaultdict
+from typing import List
+from collections import Counter
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        pCount = Counter(p)
+        window = Counter(s[:len(p)])
         res = []
-        pL, sL = len(p), len(s)
-        if sL < pL:
-            return res
-        
-        pCount = defaultdict(int)
-        sCount = defaultdict(int)
 
-        # Count the frequency of characters in p and the first pL characters in s
-        for i in range(pL):
-            pCount[p[i]] += 1
-            sCount[s[i]] += 1
-
-        # Check if the initial window is an anagram
-        if sCount == pCount:
-            res.append(0)
-
-        # Slide the window with stride 1
-        for i in range(1, sL - pL + 1):
-            # Add the rightmost character to the window
-            sCount[s[i + pL - 1]] += 1
-            # Remove the leftmost character from the window
-            sCount[s[i - 1]] -= 1
-            if sCount[s[i - 1]] == 0:
-                del sCount[s[i - 1]]
-
-            # Check if the current window is an anagram
-            if sCount == pCount:
+        for i in range(len(s)-len(p)+1):
+            if window == pCount:
                 res.append(i)
+            
+            # Update the window by removing the leftmost character and adding the next character
+            if i + len(p) < len(s):
+                window[s[i]] -= 1
+                if window[s[i]] == 0:
+                    del window[s[i]]
+                window[s[i + len(p)]] += 1
 
         return res

1021633cb49a9f4a725fa4f392aaae1d0fc8cac8
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-02-01 22:27:41 +0300

    Time: 34 ms (73.46%) | Memory: 16.5 MB (59.72%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.py
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.py
@@ -1,4 +1,17 @@
+# class Solution:
+#     def isStrictlyPalindromic(self, n: int) -> bool:
+#         return False
+#     #\U0001f923
+
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        return False
-    #\U0001f923
+        def int2base(n,b):
+            r=""
+            while n>0:
+                r+=str(n%b)
+                n//=b           
+            return int(r[-1::-1])
+        for i in range(2,n-1):
+            if int2base(n,i)!=n:
+                return False
+        return True

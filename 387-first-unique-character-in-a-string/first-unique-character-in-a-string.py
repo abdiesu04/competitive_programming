60aba11c969bbadfac90810f75333b1278104ba5
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-02-05 10:37:18 +0300

    Time: 78 ms (84.60%) | Memory: 16.9 MB (59.20%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,20 +1,8 @@
-# class Solution:
-#     def firstUniqChar(self, s: str) -> int:
-#         d = {}
-#         for i in s:
-#             d[i] = d.get(i,0) + 1
-#         for  val , key in d.items():
-#             if key == 1:
-#                 return val
-#         return -1
-
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        d = {}
-        for i in s:
-            d[i] = d.get(i, 0) + 1
-        for val, key in d.items():
-            if key == 1:
-                return s.index(val)
+        cnt = Counter(s)
+        for i in range(len(s)):
+            if cnt[s[i]] == 1:
+                return i
         return -1
         

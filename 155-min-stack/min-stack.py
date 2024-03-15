a23095404af267c55b3a8f2c7d554f257e171353
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-03-15 09:59:54 +0300

    Time: 49 ms (87.06%) | Memory: 20.2 MB (80.71%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,20 @@
+class MinStack:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, val: int) -> None:
+        self.stack1.append(val)
+        if len(self.stack2) == 0 or val <= self.stack2[-1]:
+            self.stack2.append(val)
+
+    def pop(self) -> None:
+        removed = self.stack1.pop()
+        if removed == self.stack2[-1]:
+            self.stack2.pop()
+
+    def top(self) -> int:
+        return self.stack1[-1]
+
+    def getMin(self) -> int:
+        return self.stack2[-1]

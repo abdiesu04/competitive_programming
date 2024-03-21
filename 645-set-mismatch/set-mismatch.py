345ebdbbefcf9b6d6f75411837ab6e551f68c375
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-03-21 19:28:15 +0300

    Time: 174 ms (24.84%) | Memory: 18.2 MB (53.84%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,3 +1,18 @@
 class Solution:
-    def findErrorNums(self, nums):
-        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        missing  = (n * (n + 1) // 2) - sum(set(nums))
+
+        mapp = defaultdict(int)
+        n = len(nums)
+        res = []
+        for i in range(n):
+            mapp[nums[i] - 1] += 1
+        print(mapp)
+        duplicate = None
+        for num , freq in mapp.items():
+            if freq > 1:
+                duplicate =num + 1
+
+
+        return [duplicate , missing]

ecea535bff79f585f8dad7aa1539e0e05a23b77a
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-04-13 21:56:48 +0300

    Time: 502 ms (32.01%) | Memory: 18.4 MB (9.35%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -4,9 +4,8 @@ class Solution:
             return -1
         
         n = len(grid)
-        q = deque([(0, 0)])
+        q = deque([(0, 0, 1)])
         visited = set([(0, 0)])
-        path_length = 1
         
         def inbound(r, c):
             return 0 <= r < n and 0 <= c < n
@@ -14,22 +13,17 @@ class Solution:
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (-1, -1), (1, 1), (-1, 1), (1, -1)]
         
         while q:
-            size = len(q)
+            r, c, length = q.popleft()
             
-            for _ in range(size):
-                r, c = q.popleft()
-                
-                if r == n - 1 and c == n - 1:
-                    return path_length
-                
-                for dr, dc in directions:
-                    nr = r + dr
-                    nc = c + dc
-                    
-                    if inbound(nr, nc) and grid[nr][nc] == 0 and (nr, nc) not in visited:
-                        q.append((nr, nc))
-                        visited.add((nr, nc))
+            if r == n - 1 and c == n - 1:
+                return length
             
-            path_length += 1
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+                
+                if inbound(nr, nc) and grid[nr][nc] == 0 and (nr, nc) not in visited:
+                    q.append((nr, nc, length + 1))
+                    visited.add((nr, nc))
         
         return -1

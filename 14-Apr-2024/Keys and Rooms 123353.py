ab54d03ad03de2035b38e6ba8c9a27ad8411e33a
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-04-15 01:24:10 +0300

    Created 'Keys and Rooms' by A2SV Hub

---
+++ 14-Apr-2024/Keys and Rooms 123353.py
@@ -0,0 +1,16 @@
+# Problem: Keys and Rooms - https://leetcode.com/problems/keys-and-rooms/
+
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        n = len(rooms)
+        visited = [False] * n
+        visited[0] = True
+        
+        def dfs(node):
+            for nei in rooms[node]:
+                if not visited[nei]:
+                    visited[nei] = True
+                    dfs(nei)
+        
+        dfs(0)
+        return all(visited)

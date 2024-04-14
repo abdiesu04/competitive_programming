59597f29506cb728f1fc6ee176eadbd51a0b1561
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-04-15 02:51:51 +0300

    Created 'All Nodes Distance K in Binary Tree' by A2SV Hub

---
+++ 14-Apr-2024/All Nodes Distance K in Binary Tree 123361.py
@@ -0,0 +1,42 @@
+# Problem: All Nodes Distance K in Binary Tree - https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        graph = defaultdict(list)
+        queue = deque([root])
+
+        # Building graph
+        while queue:
+            node = queue.popleft()
+
+            if node.left:
+                queue.append(node.left)
+                graph[node.val].append(node.left.val)
+                graph[node.left.val].append(node.val)
+            if node.right:
+                queue.append(node.right)
+                graph[node.val].append(node.right.val)
+                graph[node.right.val].append(node.val)
+
+        queue = deque([target.val])
+        visited = set([target.val])
+
+        # BFS
+        while k and queue:
+            k -= 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+                        visited.add(neighbor)
+
+        return list(queue)

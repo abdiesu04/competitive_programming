b51dd20b7af05ec936f162bd5adfd745f9f435a5
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-04-25 17:57:02 +0300

    Time: 63 ms (21.34%) | Memory: 17.2 MB (30.82%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for u, v in edges:
+            if self.isReachable(u, v, graph, set()):
+                return [u, v]
+            graph[u].append(v)
+            graph[v].append(u)
+    
+    def isReachable(self, src, dest, graph, visited):
+        if src == dest:
+            return True
+        visited.add(src)
+        for neighbor in graph[src]:
+            if neighbor not in visited:
+                if self.isReachable(neighbor, dest, graph, visited):
+                    return True
+        return False

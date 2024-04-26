89ade127ee0f7bf0d2c97f3ebae7a8ae70a630cb
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-04-26 13:12:15 +0300

    Created 'Lexicographically Smallest Equivalent String' by A2SV Hub

---
+++ 26-Apr-2024/Lexicographically Smallest Equivalent String 133959.py
@@ -0,0 +1,27 @@
+# Problem: Lexicographically Smallest Equivalent String - https://leetcode.com/problems/lexicographically-smallest-equivalent-string/
+
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        equivalence = {i: i for i in ascii_lowercase}
+
+        def find(x):
+            while x != equivalence[x]:
+                x = find(equivalence[x])
+            return x
+
+        def union(x, y):
+            x_root = find(x)
+            y_root = find(y)
+            if x_root > y_root:
+                equivalence[x_root] = y_root
+            else:
+                equivalence[y_root] = x_root
+
+        for i in range(len(s1)):
+            union(s1[i], s2[i])
+
+        ans = ""
+        for i in baseStr:
+            ans += find(i)
+
+        return ans

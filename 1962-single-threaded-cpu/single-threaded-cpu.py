1cf62e3168fec2ac8902b3c9500f7f7486d75a52
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-04-30 12:05:09 +0300

    Time: 1374 ms (92.97%) | Memory: 65.3 MB (59.32%) - LeetSync

---
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -0,0 +1,23 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        for i, task in enumerate(tasks):
+            task.append(i)
+
+        tasks.sort(key=lambda task: task[0])
+
+        result, min_heap = [], []
+        task_index, current_time = 0, tasks[0][0]
+        
+        while min_heap or task_index < len(tasks):
+            while task_index < len(tasks) and current_time >= tasks[task_index][0]:
+                heappush(min_heap, (tasks[task_index][1], tasks[task_index][2]))
+                task_index += 1
+
+            if not min_heap:
+                current_time = tasks[task_index][0]
+            else:
+                processing_time, index = heappop(min_heap)
+                current_time += processing_time
+                result.append(index)
+
+        return result

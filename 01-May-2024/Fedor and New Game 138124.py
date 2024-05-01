4414a15ae94e914cf4bb853601b08af803166a43
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-01 11:27:22 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 01-May-2024/Fedor and New Game 138124.py
@@ -0,0 +1,15 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n , m , k = map(int,input().split())
+arr  = []
+for _ in range(m+1):
+    n = int(input())
+    # print(bin(n))
+    arr.append(n)
+m = arr[-1]
+cnt = 0
+for i in range(len(arr)-1):
+    if bin(arr[i] ^ m).count("1") <= k:
+        cnt += 1
+print(cnt)
+    

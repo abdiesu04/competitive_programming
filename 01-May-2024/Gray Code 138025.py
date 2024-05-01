237383357183424653b5efa7656d0cfbaed4347b
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-01 10:44:28 +0300

    Created 'Gray Code' by A2SV Hub

---
+++ 01-May-2024/Gray Code 138025.py
@@ -0,0 +1,8 @@
+# Problem: Gray Code - https://leetcode.com/problems/gray-code/
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        v = []
+        for i in range(1 << n):
+            v.append(i ^ (i >> 1))
+        return v

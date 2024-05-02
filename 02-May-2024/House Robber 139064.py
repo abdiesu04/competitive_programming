98468d0576f7cc929723d40fd8334ddb3815ed7d
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-02 13:38:26 +0300

    Created 'House Robber' by A2SV Hub

---
+++ 02-May-2024/House Robber 139064.py
@@ -0,0 +1,12 @@
+# Problem: House Robber - https://leetcode.com/problems/house-robber/
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        ans = [nums[0], nums[1]]
+
+        for i in range(2,len(nums)):
+            ans.append(nums[i] + max(ans[:i-1]))
+        # print(ans)
+        return max(ans)

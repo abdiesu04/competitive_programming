f9d3090df659422936c2e789c6a7d04e6738d81e
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-02 11:28:47 +0300

    Created 'Single Number II' by A2SV Hub

---
+++ 02-May-2024/Single Number II 139003.py
@@ -0,0 +1,12 @@
+# Problem: Single Number II - https://leetcode.com/problems/single-number-ii/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_map = {num: 0 for num in nums}
+        for num in nums:
+            hash_map[num] += 1
+        for key, value in hash_map.items():
+            if value == 1:
+                return key
+
+                

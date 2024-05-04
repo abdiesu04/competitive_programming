476786a797b6fa67dd3d818a0ec7af45570c947d
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-04 17:12:16 +0300

    Time: 37 ms (61.34%) | Memory: 16.6 MB (26.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,18 +1,8 @@
-# Recurrence Solution
-# class Solution:
-#     def fib(self, n: int) -> int:
-#         if n == 0:
-#             return 0
-#         if n == 1:
-#             return n
-#         return self.fib(n-1) + self.fib(n-2)
-        
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 2:
+        if n <= 1:
             return n
-        n_0, n_1 = 0, 1
-        for i in range(2, n + 1):
-            cur = n_0 + n_1
-            n_0, n_1 = n_1, cur
-        return cur
+        a , b  = 0 , 1
+        for _ in range(2,n+1):
+            a, b  = b  ,a + b
+        return b

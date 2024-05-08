2c1780a5865ad262d663d7aad308aa32b8d15242
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-08 23:37:47 +0300

    Created 'C. Given Length and Sum of Digits...' by A2SV Hub

---
+++ 08-May-2024/C. Given Length and Sum of Digits... 143770.py
@@ -0,0 +1,30 @@
+# Problem: C. Given Length and Sum of Digits... - https://codeforces.com/contest/489/problem/C
+
+m, s = map(int, input().split())
+
+def isPos(m, s):
+    return s >= 0 and s <= 9 * m
+
+mn = ""
+rem = s
+for i in range(m):
+    for d in range(10):
+        if (i > 0 or d > 0 or (m == 1 and d == 0)) and isPos(m - i - 1, rem - d):
+            mn += str(d)
+            rem -= d
+            break
+
+mx = ""
+rem = s
+for i in range(m):
+    for d in range(9, -1, -1):
+        if isPos(m - i - 1, rem - d):
+            mx += str(d)
+            rem -= d
+            break
+
+if len(mn) != m or len(mx) != m:
+    print("-1 -1")
+else:
+    print(mn , mx)
+

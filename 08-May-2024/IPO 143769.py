01960740d2735e74508e694e20685b59684d5bf5
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-08 23:37:26 +0300

    Created 'IPO' by A2SV Hub

---
+++ 08-May-2024/IPO 143769.py
@@ -0,0 +1,21 @@
+# Problem: IPO - https://leetcode.com/problems/ipo/
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        heap1 = []
+        heap2 = []
+
+        for i in range(len(profits)):
+            heappush(heap1, (capital[i], profits[i]))
+
+        for _ in range(k):
+            while heap1 and heap1[0][0] <= w:
+                _, profit = heappop(heap1)
+                heappush(heap2, -profit)
+
+            if heap2:
+                w -= heappop(heap2) 
+            else:
+                break
+          
+        return w

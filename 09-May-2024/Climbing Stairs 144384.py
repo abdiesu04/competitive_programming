e23601ac1f63b7502c8dea7130787a1a323a10d8
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-09 19:32:59 +0300

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 09-May-2024/Climbing Stairs 144384.py
@@ -0,0 +1,8 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one , two = 1 , 1
+        for i in range(n-1):
+            one , two = two , one + two
+        return two

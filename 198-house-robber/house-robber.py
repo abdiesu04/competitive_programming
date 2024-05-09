07fadc4a4e3df16cf5beeb032935591f7e6b598a
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-09 19:13:32 +0300

    Time: 23 ms (99.17%) | Memory: 16.5 MB (44.34%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,7 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
+        if len(nums) == 1:
+            return nums[0]
         ans = [nums[0], nums[1]]
+
         for i in range(2,len(nums)):
             ans.append(nums[i] + max(ans[:i-1]))
+        # print(ans)
         return max(ans)

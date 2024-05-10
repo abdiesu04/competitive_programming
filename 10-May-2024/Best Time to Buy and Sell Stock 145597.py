d5baeea58216e8b03b6eb2381f8f277b62a0e8ae
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-10 16:31:22 +0300

    Created 'Best Time to Buy and Sell Stock' by A2SV Hub

---
+++ 10-May-2024/Best Time to Buy and Sell Stock 145597.py
@@ -0,0 +1,13 @@
+# Problem: Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            max_profit = max( max_profit , price - min_price )
+        return max_profit
+        

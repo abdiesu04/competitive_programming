257c77b96a8fc865dfd4312b4fc44f3d85ac6be5
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-12 11:50:32 +0300

    Time: 452 ms (81.84%) | Memory: 41.8 MB (69.53%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -6,7 +6,9 @@ class Solution:
         
         for i in range(1 , m+1):
             for j in range(1,n+1):
+                
                 if t1[i-1] == t2[j-1]:
+                    
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])

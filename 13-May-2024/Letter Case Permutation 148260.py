51cb1680fd7d9238038372676e934ff62631cc46
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-13 22:01:24 +0300

    Created 'Letter Case Permutation' by A2SV Hub

---
+++ 13-May-2024/Letter Case Permutation 148260.py
@@ -0,0 +1,19 @@
+# Problem: Letter Case Permutation - https://leetcode.com/problems/letter-case-permutation/
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        def backtrack(sub="", i=0, res=None):
+            if res is None:
+                res = []
+            
+            if len(sub) == len(s):
+                res.append(sub)
+            else:
+                if s[i].isalpha():
+                    backtrack(sub + s[i].swapcase(), i + 1, res)
+                backtrack(sub + s[i], i + 1, res)
+            
+            return res
+        
+        return backtrack()
+

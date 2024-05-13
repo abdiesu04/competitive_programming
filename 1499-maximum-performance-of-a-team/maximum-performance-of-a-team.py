4e848dc23e064d75d1d229333a5996659739ab9d
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-13 21:43:25 +0300

    Time: 268 ms (95.52%) | Memory: 32.2 MB (85.52%) - LeetSync

---
+++ 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        
+        tot_spd, h = 0, []
+        ans = -float('inf')
+        
+        for i, spd in sorted(zip(efficiency, speed),reverse=True):
+            while len(h) > k-1:
+                tot_spd -= heappop(h)
+            heappush(h, spd)
+            tot_spd += spd
+            ans = max(ans, tot_spd * i)
+            
+        return ans % (10**9+7)

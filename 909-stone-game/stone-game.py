3f03377692424f37fb8b823168cfa85e340d7619
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-14 19:41:27 +0300

    Time: 41 ms (68.77%) | Memory: 16.5 MB (84.24%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,28 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        memo = {} 
-        
-        def dp(l, r, alice, bob):
-            if l == r:
-                return alice > bob
-
-            if (l, r, alice, bob) in memo:
-                return memo[(l, r, alice, bob)]
-            
-            if dp(l + 1, r, alice + piles[l], bob):
-                memo[(l, r, alice, bob)] = True
-                return True
-            if dp(l, r - 1, alice + piles[r], bob):
-                memo[(l, r, alice, bob)] = True
-                return True
-            if dp(l + 1, r, alice, bob + piles[l]):
-                memo[(l, r, alice, bob)] = True
-                return True
-            if dp(l, r - 1, alice, bob + piles[r]):
-                memo[(l, r, alice, bob)] = True
-                return True
-            
-            memo[(l, r, alice, bob)] = False
-            return False
-        
-        return dp(0, len(piles) - 1, 0, 0)
+        return True

86c1b1b427e7f2fa91ab8789faecedb3f0ca1db6
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-14 09:55:56 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 14-May-2024/Cirno's Perfect Bitmasks Classroom 148532.py
@@ -0,0 +1,8 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+for t in range(int(input())):
+	a=int(input())
+	b=a&-a
+	while (a==b or (a&b)==0):
+		b+=1
+	print(b)

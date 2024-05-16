09914cdad03224630c31a28a85a3c624a28021d7
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-16 11:32:26 +0300

    Created 'Palindrome Partitioning' by A2SV Hub

---
+++ 16-May-2024/Palindrome Partitioning 151873.py
@@ -0,0 +1,20 @@
+# Problem: Palindrome Partitioning - https://leetcode.com/problems/palindrome-partitioning/
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        par = []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(par.copy())
+                return
+
+            for j in range(i, len(s)):
+                if s[i:j+1] == s[i:j+1][::-1]:
+                    par.append(s[i:j+1])
+                    dfs(j+1)
+                    par.pop()
+
+        dfs(0)
+        return res

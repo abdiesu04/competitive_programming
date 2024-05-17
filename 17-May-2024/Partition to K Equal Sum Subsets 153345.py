41cac29beed9f0ed8c8059b16f234ae7148c6af5
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-17 19:31:40 +0300

    Created 'Partition to K Equal Sum Subsets' by A2SV Hub

---
+++ 17-May-2024/Partition to K Equal Sum Subsets 153345.py
@@ -0,0 +1,30 @@
+# Problem: Partition to K Equal Sum Subsets - https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+
+class Solution:
+    def canPartitionKSubsets(self, nums, k):
+        total_sum = sum(nums)
+        if total_sum % k != 0:
+            return False
+
+        target_sum = total_sum // k
+        subset_sums = [0] * k
+
+        def backtrack(index):
+            if index == len(nums):
+                return True
+
+            num = nums[index]
+            for i in range(k):
+                if subset_sums[i] + num <= target_sum:
+                    subset_sums[i] += num
+                    if backtrack(index + 1):
+                        return True
+                    subset_sums[i] -= num
+
+                    if subset_sums[i] == 0:
+                        break
+
+            return False
+
+        nums.sort(reverse=True) 
+        return backtrack(0)

bea2d103d5e136f186048e176ca226c1c6c374e3
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-18 09:12:43 +0300

    Created 'Solving Questions with Brainpower' by A2SV Hub

---
+++ 18-May-2024/Solving Questions with Brainpower 153844.py
@@ -0,0 +1,12 @@
+# Problem: Solving Questions with Brainpower - https://leetcode.com/problems/solving-questions-with-brainpower/
+
+class Solution:
+    def mostPoints(self, qns: list[list[int]]) -> int:
+        @cache
+        def go(i: int) -> int:
+            if i >= len(qns):
+                return 0
+            return max(qns[i][0] + go(i + 1 + qns[i][1]), go(i + 1))
+           
+        
+        return go(0)

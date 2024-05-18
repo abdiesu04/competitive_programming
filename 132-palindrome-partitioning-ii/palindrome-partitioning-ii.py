df271e5711807b885c07842750cc1497a8267c98
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-18 09:59:45 +0300

    Time: 3525 ms (16.62%) | Memory: 18.7 MB (68.69%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minCut(self, s: str) -> int:
+        @cache
+        def dp(start):
+            if start == len(s):
+                return -1 
+
+            minCuts = float('inf')  
+
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] == s[start:end][::-1]:
+                    cuts = 1 + dp(end)  
+                    minCuts = min(minCuts, cuts)
+ 
+            return minCuts
+
+        return dp(0)

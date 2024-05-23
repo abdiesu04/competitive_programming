5a751f3dbb3079afe7e1feba44cab11e53990cfe
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-23 17:47:17 +0300

    Created 'Longest Common Prefix' by A2SV Hub

---
+++ 23-May-2024/Longest Common Prefix 157899.py
@@ -0,0 +1,31 @@
+# Problem: Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Solution:
+    def insert(self, root: TrieNode, word: str) -> None:
+        node = root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+        
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        root = TrieNode()
+        for word in strs:
+            self.insert(root, word)
+
+        node = root
+        prefix = ""
+        while len(node.children) == 1 and not node.is_end:
+            for char in node.children:
+                node = node.children[char]
+                prefix += char
+        return prefix

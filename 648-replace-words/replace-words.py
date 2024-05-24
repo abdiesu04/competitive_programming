108b55bd8d319d4e1209d98ab1c650513a9de639
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-24 20:23:45 +0300

    Time: 103 ms (68.50%) | Memory: 32.7 MB (42.61%) - LeetSync

---
+++ 648-replace-words/replace-words.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word):
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
+        cur.isEnd = True
+        
+    def search(self, word):
+        cur = self.root
+        res = ''
+        for char in word:
+            if char in cur.children:
+                res += char
+                cur = cur.children[char]
+                if cur.isEnd:
+                    break
+            else:
+                break
+        return res if cur.isEnd else word
+        
+class Solution:
+    def replaceWords(self, roots: List[str], sentence: str) -> str:
+        t = Trie()
+        for root in roots:
+            t.insert(root)
+        return ' '.join(t.search(word) for word in sentence.split())

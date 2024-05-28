f05f2b9118379d3d05ebac50ef1014846aa11708
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-28 17:54:09 +0300

    Time: 586 ms (54.68%) | Memory: 20.4 MB (20.98%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, pre: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = defaultdict(list)
+        required = defaultdict(set)
+        for u,v in pre:
+            graph[v].append(u)
+    
+        visited = set()
+        def dfs(i,s):
+            visited.add(i)
+            required[s].add(i)
+            for v in graph[i]:
+                if v not in visited:
+                    dfs(v,s)
+        for i in range(numCourses):
+            visited.clear()
+            dfs(i,i)
+        ans = []
+        for u,v in queries:
+            ans.append((u in required[v]))
+        return ans

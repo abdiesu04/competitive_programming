45285f6f2c33720294dae48f0675c0ea869155fc
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-05-31 19:31:05 +0300

    Time: 50 ms (92.12%) | Memory: 18.7 MB (30.48%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -1,10 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        p=0
-        q=0
-        for i in nums :
-            p^=i
+        mapp = defaultdict(int)
         for i in nums:
-            if ((p&(-p))&i)==0 :
-                q^=i
-        return [p^q,q];
+            mapp[i] += 1
+        res = []
+        for key , value in mapp.items():
+            if value == 1:
+                res.append(key)
+        return res
+
+        

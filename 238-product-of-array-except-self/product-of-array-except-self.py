1479295a326839d41d927d62eac7f2a7ce03cd93
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-06-01 11:29:01 +0300

    Time: 282 ms (24.82%) | Memory: 26.1 MB (32.71%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,40 +1,19 @@
-# class Solution:
-#     def productExceptSelf(self, nums: List[int]) -> List[int]:
-#         length=len(nums)
-#         sol=[1]*length
-#         pre = 1
-#         post = 1
-#         for i in range(length):
-#             sol[i] *= pre
-#             pre = pre*nums[i]
-#             sol[length-i-1] *= post
-#             post = post*nums[length-i-1]
-#         return(sol)
-
-from typing import List
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        post = [1] * len(nums)
-        pre = [1] * len(nums)
-        
-        s = 1
+        prefix = [1]
+        postfix = [1] * (len(nums) + 1)
+        accumulator  = 1
         for i in range(len(nums)):
-            s *= nums[i]
-            pre[i] = s
-        
-        s = 1
-        for i in range(len(nums) - 1, -1, -1):
-            s *= nums[i]
-            post[i] = s
-        
-        res = []
-        for i in range(len(nums)):
-            if i == 0:
-                res.append(post[i + 1])
-            elif i == len(nums) - 1:
-                res.append(pre[i - 1])
-            else:
-                res.append(pre[i - 1] * post[i + 1])
-        
-        return res
+            accumulator  *= nums[i]
+            prefix.append(accumulator)
+        accumulator  = 1
+        for i in range(len(nums)-1 , -1 , -1):
+            accumulator  *= nums[i]
+            postfix[i] = accumulator
+        print(prefix)
+        print(postfix)
+        result = []
+        for i in range(len(prefix) -1):
+            result.append(prefix[i] * postfix[i+1])
+
+        return result

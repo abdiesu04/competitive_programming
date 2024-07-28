db9b490e45e06019e5b8c43a71867a475457c50e
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-07-29 00:43:42 +0300

    Time: 160 ms (46.04%) | Memory: 18 MB (17.16%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
@@ -1,23 +1,15 @@
-# class Solution:
-#     def largestPerimeter(self, nums: List[int]) -> int:
-#         nums.sort()
-#         mx = 0
-#         for i in range(len(nums)-1,1, -1):
-#             if nums[i-2] + nums[i-1] > nums[i]:
-#                 mx = max(mx , nums[i-2] + nums[i-1] + nums[i] )
-#         return mx
-
-class Solution(object):
-    def largestPerimeter(self, A):
-        A.sort(reverse=True)
-
-        for i in range(len(A)-2):
-            a = A[i]
-            b= A[i+1]
-            c = A[i+2]
-            if a< b+ c:
-                return a+b+c
-            
-        return 0
-
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse = True)
         
+        def check(a , b, c):
+            return (
+                a + b > c and
+                a + c > b and
+                b + c > a
+            )
+
+        for i in range(len(nums) - 2):
+            if check(nums[i] , nums[i+1] , nums[i+2]):
+                return nums[i] + nums[i+1] + nums[i+2]
+        return 0

5a1a79134bb8a3dbf1abd038c46d7bda9ebc0f8f
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-09-06 09:23:43 +0300

    Created 'Sum of Digits of String After Convert' by A2SV Hub

---
+++ 06-Sep-2024/Sum of Digits of String After Convert 211520.py
@@ -0,0 +1,27 @@
+# Problem: Sum of Digits of String After Convert - https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        s = self.convert(s)
+        for i in range(k):
+            s  = self.transform(s)
+
+        return int(s)
+        
+
+    def stringToNumber(self , s):
+        return ord(s) - ord("a") + 1
+        
+    def convert(self , s):
+        total = ''
+        for i in s:
+            total += str(self.stringToNumber(i) )
+           
+        return total
+
+    def transform(self , s):
+        result = 0
+        for i in s:
+            result += int(i)
+        return str(result)
+

da1423b591f86aec29dcc7d92b7ec5ba2ff92d6f
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-09-09 13:08:04 +0300

    Time: 34 ms (68.30%) | Memory: 16.6 MB (12.41%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        mod = (10 ** 9 ) + 7
+        even_cnt  = n // 2 if n % 2 == 0 else n // 2 + 1
+        odd_cnt = n // 2
+
+        res = pow(5 , even_cnt , mod) * pow(4 , odd_cnt, mod)
+        return res % mod

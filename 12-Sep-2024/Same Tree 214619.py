73d8993c7b1428230ef52f3993f5a269bed293dc
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-09-12 20:36:03 +0300

    Created 'Same Tree' by A2SV Hub

---
+++ 12-Sep-2024/Same Tree 214619.py
@@ -0,0 +1,25 @@
+# Problem: Same Tree - https://leetcode.com/problems/same-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        ans = True
+        def dfs(node1 , node2):
+            nonlocal ans
+            if not node1 and not node2:
+                return
+            if not node1 or not node2:
+                ans = False
+                return 
+            if node1.val != node2.val:
+                ans = False
+            dfs(node1.left , node2.left)
+            dfs(node1.right , node2.right)
+
+        dfs(p , q)
+        return ans

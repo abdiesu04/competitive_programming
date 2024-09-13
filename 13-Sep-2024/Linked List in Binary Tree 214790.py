ef9716e9e7828a3f4805f01ac250e38166c4f26e
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-09-13 11:43:30 +0300

    Created 'Linked List in Binary Tree' by A2SV Hub

---
+++ 13-Sep-2024/Linked List in Binary Tree 214790.py
@@ -0,0 +1,41 @@
+# Problem: Linked List in Binary Tree - https://leetcode.com/problems/linked-list-in-binary-tree/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+
+        def checkpath(node , temp):
+            if not temp:
+                return True
+
+            if not node:
+                return False
+
+            if node.val == temp.val:
+                return checkpath(node.left , temp.next) or checkpath(node.right , temp.next)
+            return False
+
+        def dfs(node):
+            if not node:
+                return False
+
+            if node.val == head.val and checkpath(node , head):
+                return True
+
+            return dfs(node.right) or dfs(node.left)
+
+        return dfs(root)
+
+
+
+            

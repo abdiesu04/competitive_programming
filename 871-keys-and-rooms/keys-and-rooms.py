1d3e78fae62f128565145987ab6d0c70414c049b
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-10-14 11:07:59 +0300

    Time: 69 ms (46.92%) | Memory: 17.1 MB (66.86%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,14 +1,20 @@
-class Solution(object):
-    def canVisitAllRooms(self, rooms):
-        n = len(rooms)
-        visited = [False] * n
-        visited[0] = True
-        
-        def dfs(node):
-            for nei in rooms[node]:
-                if not visited[nei]:
-                    visited[nei] = True
-                    dfs(nei)
-        
-        dfs(0)
-        return all(visited)
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+
+        for i in range(len(rooms)):
+            for j in range(len(rooms[i])):
+                graph[i].append(rooms[i][j])
+
+        queue = deque([0])
+        visited  = set([0])
+
+        while queue:
+            node = queue.popleft()
+            visited.add(node)
+            for child in graph[node]:
+                if child not in visited:
+                    queue.append(child)
+
+        return len(visited) == len(rooms)
+

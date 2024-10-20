4e27d67f163106eee01854f9d66998e7d3de8d03
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-10-21 00:28:10 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (6.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-        def dp(num):
-            if num == n:
-                return 1
-            if num > n:
-                return 0
-            if num not in memo:
-                memo[num] = dp(num + 1) + dp(num + 2)
-            return memo[num]
-        return dp(0)
-
-            
+        if n == 1:
+            return 1
+        dp = [0] * n  # Initialize the dp array with n elements
+        dp[n-1] = 1  # There's only 1 way to climb the last step
+        dp[n-2] = 1  # There's only 1 way to climb if you're 2 steps away from the top
+        
+        for i in range(n-3, -1, -1):  # Iterate backwards from n-3 to 0
+            dp[i] = dp[i+1] + dp[i+2]  # Sum the ways from the next two steps
+        print(dp)
+        return dp[0] + dp[1]

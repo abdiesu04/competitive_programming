bf281bc4b20436340e3d50f435bc992fb1408677
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-12-18 21:37:29 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (10.22%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        result = prices.copy()
+
+        stack = deque()
+
+        for i in range(len(prices)):
+            while stack and prices[stack[-1]] >= prices[i]:
+                result[stack.pop()] -= prices[i]
+            stack.append(i)
+
+        return result

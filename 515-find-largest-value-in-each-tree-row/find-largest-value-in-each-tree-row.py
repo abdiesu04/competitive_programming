db71c82e0dfe51783f83b44c417aa2cd9b97945d
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-12-25 08:45:52 +0300

    Time: 3 ms (48.85%) | Memory: 19.8 MB (6.38%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -6,19 +6,38 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        que = deque([root])
-        res  = []
-        while que:
-            mx = float('-inf')
-            for _ in range(len(que)):
-                node  = que.popleft()
-                mx  = max(mx , node.val)
-                if node.left:
-                    que.append(node.left)
-                if node.right:
-                    que.append(node.right)
-            res.append(mx)
+    #     if not root:
+    #         return []
+    #     que = deque([root])
+    #     res  = []
+    #     while que:
+    #         mx = float('-inf')
+    #         for _ in range(len(que)):
+    #             node  = que.popleft()
+    #             mx  = max(mx , node.val)
+    #             if node.left:
+    #                 que.append(node.left)
+    #             if node.right:
+    #                 que.append(node.right)
+    #         res.append(mx)
+    #     return res
+        mapp = {}
+        def dfs(node , level):
+            if not node:
+                return 
+            if level not in mapp:
+                mapp[level] = float('-inf')
+            mapp[level] = max(mapp[level] , node.val)
+
+            if node.right:
+                dfs(node.right , level + 1)
+            if node.left:
+                dfs(node.left , level + 1)
+
+        dfs(root , 0)
+        res = []
+        for _ , val in mapp.items():
+            res.append(val)
         return res
+            
 

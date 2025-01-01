a75f755f7f602f8ec6ac8f32c39a212189a853c1
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2025-01-01 10:56:52 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (6.31%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,30 +1,28 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         graph = defaultdict(list)
+        
+        for i, (u, v) in enumerate(equations):
+            graph[u].append((v, values[i]))
+            graph[v].append((u, 1 / values[i]))
 
-        for i ,( u , v) in enumerate(equations):
-            graph[u].append((v , values[i]))
-            graph[v].append((u , 1 / values[i]))
-
-        def dfs(node , result , end , visited):
+        def dfs(node, end, val, visited):
             if node == end:
-                return result
+                return val
             visited.add(node)
-
-            for child , val in graph[node]:
-                if child not in visited:
-                    found = dfs(child , result * val , end , visited)
-                    if found != -1:
-                        return found
-            return -1
-                
-
-        res = []
-        for start , end in queries:
-            if start not in graph or end not in graph:
-                res.append(-1.0)
-            else:
-                ans = dfs(start , 1 , end , set())
-                res.append(ans if ans != -1 else -1.0)
-        return res
             
+            for child, new_val in graph[node]:
+                if child not in visited:
+                    res = dfs(child, end, val * new_val, visited)  
+                    if res != -1:
+                        return res
+            return -1
+        
+        res = []
+        for start, end in queries:
+            if start not in graph or end not in graph:
+                res.append(-1)
+            else:
+                res.append(dfs(start, end, 1, set()))  
+        
+        return res

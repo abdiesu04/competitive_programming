9f7fe73ce2ab6911676d06aaf962084f47d24c89
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2025-01-08 11:02:28 -0800

    Time: 1756 ms (57.21%) | Memory: 18.3 MB (11.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,17 +1,10 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        """
-        Aproach:
-        for every number start with dp array all one assuming every number has atleast one increasing subsequence
-        ending with it
-        and update the dp based on the previous subsequence ending with it
-        """
-
         dp = [1] * len(nums)
-    
-        for i in range(len(nums)):
-            for j  in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[j] + 1 , dp[i])
-
+        for i in range(1,len(nums)):
+            temp = 1
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    temp = max(temp , dp[j] + 1)
+            dp[i] = temp
         return max(dp)
